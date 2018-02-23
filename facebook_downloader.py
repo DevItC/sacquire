95a4917fc5e94b9c32116662bc98bd1685862e72
Aniket Kumar <cs5160398@cse.iitd.ac.in>
2018-02-23 22:16:36 +0530

    decorated slightly

--- facebook_downloader.py
+++ facebook_downloader.py
@@ -1,30 +1,31 @@
-import requests, bs4, os
+import requests
 from selenium import webdriver
 
-link = "https://m.facebook.com/479753488786510/photos/a.480062335422292.1073741828.479753488786510/1616462925115555/"
 
-options = webdriver.ChromeOptions()
-options.add_argument("headless")
-driver = webdriver.Chrome(chrome_options=options)
-driver.get(link)
+def download(url):
+    options = webdriver.ChromeOptions()
+    options.add_argument("headless")
+    driver = webdriver.Chrome(chrome_options=options)
+    driver.get(url)
+    elem = driver.find_elements_by_class_name("sec")
+    image_link = elem[1].get_attribute("href")
+    driver.close()
 
-elem = driver.find_elements_by_class_name("sec")
-image_link = elem[1].get_attribute("href")
-driver.close()
-# print(image_link)
+    res = requests.get(image_link)
+    res.raise_for_status()
+    title = image_link.split("?")[0].split("/")[-1]
+    print(title)
+    imageFile = open(title, 'wb')
+    for chunk in res.iter_content(100000):
+        imageFile.write(chunk)
+    imageFile.close()
+    return title
 
 
-res = requests.get(image_link)
-res.raise_for_status()
-soup = bs4.BeautifulSoup(res.text, "html.parser")
-# print(soup.select('head title')[0].text)
-# title = soup.find('title')
-title = image_link.split("?")[0].split("/")[-1]
-print (title)
-imageFile = open(title, 'wb')
+def main():
+    url = input("Enter image url: ")
+    url = "m".join(url.split("www"))
+    download(url)
 
-for chunk in res.iter_content(100000):
-    imageFile.write(chunk)
 
-imageFile.close()
 
