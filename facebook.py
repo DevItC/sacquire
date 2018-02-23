c014fb40df59d1f2c8cb4fc0fc32a2f0ff077056
Sumit Ghosh <sumit.ghosh32@gmail.com>
2018-02-23 22:27:17 +0530

    Complete the module facebook

--- facebook.py
+++ facebook.py
@@ -2,10 +2,13 @@ import requests
 from selenium import webdriver
 
 
+
 def download(url):
     options = webdriver.ChromeOptions()
     options.add_argument("headless")
     driver = webdriver.Chrome(chrome_options=options)
+
+    url = "m".join(url.split("www"))
     driver.get(url)
     elem = driver.find_elements_by_class_name("sec")
     image_link = elem[1].get_attribute("href")
@@ -14,18 +17,18 @@ def download(url):
     res = requests.get(image_link)
     res.raise_for_status()
     title = image_link.split("?")[0].split("/")[-1]
-    print(title)
-    imageFile = open(title, 'wb')
+    image_file = open(title, "wb")
     for chunk in res.iter_content(100000):
-        imageFile.write(chunk)
-    imageFile.close()
+        image_file.write(chunk)
+    image_file.close()
     return title
 
 
 def main():
-    url = input("Enter image url: ")
-    url = "m".join(url.split("www"))
-    download(url)
+    url = input("[*] Enter image URL: ")
+    filename = download(url)
+    print("[*] Image saved as {}".format(filename))
 
 
-
+if __name__=="__main__":
+	main()
