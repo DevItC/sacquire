cb951e76b9cfe518fcd1e8b142895d511ec85de4
Aniket Kumar <cs5160398@cse.iitd.ac.in>
2018-02-23 23:58:58 +0530

    Combine image and video download

--- facebook.py
+++ facebook.py
@@ -1,39 +1,46 @@
 import os
 import requests
+import json
 from selenium import webdriver
 
-
-
 def download(url):
-    if not os.path.isdir("images"):
-        os.makedirs("images")
-
     options = webdriver.ChromeOptions()
     options.add_argument("headless")
     driver = webdriver.Chrome(chrome_options=options)
 
     url = "m".join(url.split("www"))
     driver.get(url)
-    elem = driver.find_elements_by_class_name("sec")
-    image_link = elem[1].get_attribute("href")
+    try:
+        elem = driver.find_element_by_xpath("//div[@class='widePic']/div").get_attribute("data-store")
+        link = json.loads(elem)["src"]
+        type = "videos"
+    except:
+        elem = driver.find_elements_by_class_name("sec")
+        link = elem[1].get_attribute("href")
+        type = "images"
+        pass
     driver.close()
 
-    res = requests.get(image_link)
+    res = requests.get(link)
     res.raise_for_status()
-    title = image_link.split("?")[0].split("/")[-1]
+    title = link.split("?")[0].split("/")[-1]
     
-    with open(os.path.join("images", title), "wb") as image:
+    if not os.path.isdir(type):
+        os.makedirs(type)
+        
+    with open(os.path.join(type, title), "wb") as file:
         for chunk in res.iter_content(100000):
-            image.write(chunk)
-    
+            file.write(chunk)
+
     return title
+
 
 
 def main():
     url = input("[*] Enter image URL: ")
     filename = download(url)
-    print("[*] Image saved as {}".format(filename))
+    print("[*] File saved as {}".format(filename))
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
