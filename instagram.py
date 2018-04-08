a826d50b698937fbca629a5b3ace1ea5eaa997dd
Maderana <maderanaabhishek@gmail.com>
2018-04-08 18:27:29 +0530

    insta downloader done

--- instagram.py
+++ instagram.py
@@ -7,16 +7,60 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-def download_link(url):
+def download(driver):
+	try:
+		elem = driver.find_element_by_class_name('_l6uaz').get_attribute("src")
+		link = elem
+		dirname = "videos"
+	except:
+		elem = driver.find_element_by_class_name('_2di5p').get_attribute('src')
+		link = elem
+		dirname = "images"
+	driver.close()
+
+	res = requests.get(link)
+	res.raise_for_status()
+	title = link.split("/")[-1]
+
+	if not os.path.isdir(dirname):
+		os.makedirs(dirname)
+
+	with open(os.path.join(dirname, title), "wb") as file:
+		for chunk in res.iter_content(100000):
+			file.write(chunk)
+
+	return os.path.join(dirname, title)
 
 
-def preview(url):
-	url_token = url.split('/')[4]
-	return "https://www.instagram.com/p/{}/".format(url_token)
+
+def get_preview(driver):
+	try:
+		element = driver.find_element_by_class_name('_l6uaz').get_attribute("poster")
+		url = element
+		url_type = "video"
+	except:
+		url_type = "image"
+		element = driver.find_element_by_class_name('_2di5p').get_attribute("srcset")
+		url = elememt.split(".jpg")[0] + ".jpg"
+	return [url_type, url]
+		
+
+def open_link(url):
+	options = webdriver.ChromeOptions()
+	options.add_argument("headless")
+	options.add_argument('--no-sandbox')
+	driver = webdriver.Chrome()
+	driver.get(url)
+	return driver
 
 def main():
     url = input("[*] Enter URL for picture/video: ")
-    image_url = preview(url)
+    driver = open_link(url)
+    
+    preview = get_preview(driver)
+    print (preview)
+
+    link = download(driver)
 
 
 if __name__ == "__main__":
