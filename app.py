a8cea59969f8e909e9711c583a1c91ac13e12821
Aniket Kumar <cs5160398@cse.iitd.ac.in>
2018-04-16 12:23:11 +0530

    done loading preview.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from rq import Queue
 from rq.job import Job
 from worker import conn
 from flask import Flask, render_template, request, send_file, jsonify, redirect, url_for
-
+import time
 
 app = Flask(__name__)
 q = Queue(connection=conn)
@@ -11,14 +11,14 @@ q = Queue(connection=conn)
 
 @app.route('/home', methods=['GET'])
 def index():
-    image = request.args.get('image')
+    # image = request.args.get('image')
     preview_link = request.args.get('preview_link')
     link = request.args.get('link')
-    return render_template('form.html', image=image, preview_link=preview_link, link=link )
+    return render_template('form.html', preview_link=preview_link, link=link )
 
 @app.route('/')
 def index2():
-    return redirect(url_for('index', image=False, preview_link="", link=""))
+    return redirect(url_for('index', preview_link="", link=""))
 
 @app.route('/enqueue', methods=['POST'])
 def enqueue():
@@ -37,8 +37,8 @@ def enqueue():
 @app.route('/tasks/<task_id>', methods=['GET'])
 def get_status(task_id):
     task = q.fetch_job(task_id)
-
     if task:
+        status = task.get_status()
         response = {
             'status': 'success',
             'data': {
@@ -46,18 +46,27 @@ def get_status(task_id):
                 'task_status': task.get_status(),
             }
         }
+        if (status=='finished'):
+            result = q.fetch_job(task_id).result
+            response['data']['preview_link'] = result[0][1]
+            response['data']['link'] = result[1][1]
     else:
         response = {'status': 'error'}
 
     return jsonify(response)
 
 
-@app.route('/download/<task_id>')
-def preview(task_id):
-    result = q.fetch_job(task_id).result
-
-    return redirect(url_for('index', image=True, preview_link=result[0][1], link=result[1][1]))
-
+# @app.route('/download/<task_id>', methods=['GET'])
+# def preview(task_id):
+#     result = q.fetch_job(task_id).result
+#     if result:
+#         response = {
+#             'image' : True,
+#             'preview_link' : result[0][1],
+#             'link' : result[1][1],
+#         }
+#     # return redirect(url_for('index', image=True, preview_link=result[0][1], link=result[1][1]))
+#     return jsonify(response)
 
 # @app.route('/download/<task_id>', methods=['GET'])
 # def download(task_id):
--- static/scripts/main.js
+++ static/scripts/main.js
@@ -36,15 +36,54 @@ function getStatus(taskID) {
     const taskStatus = res.data.task_status;
 
     if (taskStatus === 'finished' || taskStatus === 'failed') {
-      window.location.assign(`/download/${taskID}`);
+      console.log('Completion check')
+      var elem = document.querySelector('#link_im')
+      elem.setAttribute("href", res.data.link);
+      elem = document.querySelector('#img_src')
+      elem.setAttribute("src", res.data.preview_link);
+      
+      // elem.innerHTML.innerHTML.src = res.data.preview_link;
+      elem = document.querySelector('#image')
+      
+      elem.style.display = "Block";
+      console.log('JQuery set check.')
+      elem = document.getElementById("image")
+      window.history.replaceState({}, "", window.location.href);
+
       l.stop();
       return false;
     }
     setTimeout(function() {
       getStatus(res.data.task_id);
-    }, 1000);
-  })
+    }, 1000)
+ ; })
   .fail((err) => {
     console.log(err)
   })
+}
+
+function updateQuery(key, value){
+  url = window.location.href
+
+}
+
+function updateQueryStringParam(key, value) {
+  param = location.protocol
+  baseUrl = [location.protocol, '//', location.host, location.pathname].join('');
+  console.log(baseUrl)
+  urlQueryString = document.location.search;
+  var newParam = key + '=' + value,
+  params = '?' + newParam;
+
+  // If the "search" string exists, then build params from it
+  if (urlQueryString) {
+    keyRegex = new RegExp('([\?&])' + key + '[^&]*');
+    // If param exists already, update it
+    if (urlQueryString.match(keyRegex) !== null) {
+      params = urlQueryString.replace(keyRegex, "$1" + newParam);
+    } else { // Otherwise, add it to end of query string
+      params = urlQueryString + '&' + newParam;
+    }
+  }
+  window.history.replaceState({}, "", baseUrl + params);
 }
