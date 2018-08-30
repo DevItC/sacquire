9f2a19119ffb0eae5852711941ff9fc31ab8ccf6
Sumit Ghosh <sumit@sumit-ghosh.com>
2018-08-30 23:14:57 +0530

    Remove worker and RQ. Do it quickly quickly!

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -13,77 +13,28 @@ $('form').submit(function(event) {
 
   $.ajax(
     {
-      url: '/enqueue',
+      url: '/api',
       data: formdata,
       type: 'POST',
       dataType: 'JSON'
     }
   )
   .done((res) => {
-    getStatus(res.data.task_id)
+    console.log('Completion check')
+    var elem = document.querySelector('#link_im')
+    elem.setAttribute("href", res.media_url);
+    elem = document.querySelector('#img_src')
+    elem.setAttribute("src", res.media_preview);
+    elem = document.querySelector('#image')    
+    elem.style.display = "Block";
+    console.log('JQuery set check.')
+    elem = document.getElementById("image")
+    window.history.replaceState({}, "", window.location.href);
+
+    l.stop();
+    return false;
   })
   .fail((err) => {
     console.log(err)
   })
 })
-
-function getStatus(taskID) {
-  $.ajax({
-    url: `/tasks/${taskID}`,
-    method: 'GET'
-  })
-  .done((res) => {
-    const taskStatus = res.data.task_status;
-
-    if (taskStatus === 'finished' || taskStatus === 'failed') {
-      console.log('Completion check')
-      var elem = document.querySelector('#link_im')
-      elem.setAttribute("href", res.data.media_url);
-      elem = document.querySelector('#img_src')
-      elem.setAttribute("src", res.data.media_preview);
-      
-      // elem.innerHTML.innerHTML.src = res.data.media_preview;
-      elem = document.querySelector('#image')
-      
-      elem.style.display = "Block";
-      console.log('JQuery set check.')
-      elem = document.getElementById("image")
-      window.history.replaceState({}, "", window.location.href);
-
-      l.stop();
-      return false;
-    }
-    setTimeout(function() {
-      getStatus(res.data.task_id);
-    }, 1000)
- ; })
-  .fail((err) => {
-    console.log(err)
-  })
-}
-
-function updateQuery(key, value){
-  url = window.location.href
-
-}
-
-function updateQueryStringParam(key, value) {
-  param = location.protocol
-  baseUrl = [location.protocol, '//', location.host, location.pathname].join('');
-  console.log(baseUrl)
-  urlQueryString = document.location.search;
-  var newParam = key + '=' + value,
-  params = '?' + newParam;
-
-  // If the "search" string exists, then build params from it
-  if (urlQueryString) {
-    keyRegex = new RegExp('([\?&])' + key + '[^&]*');
-    // If param exists already, update it
-    if (urlQueryString.match(keyRegex) !== null) {
-      params = urlQueryString.replace(keyRegex, "$1" + newParam);
-    } else { // Otherwise, add it to end of query string
-      params = urlQueryString + '&' + newParam;
-    }
-  }
-  window.history.replaceState({}, "", baseUrl + params);
-}
